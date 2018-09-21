0f2603c098bcf85be19885b3677c9c284b161389
unTsar <bycucaka@gmail.com>
2018-09-21 23:09:40 +0300

    666
    666

--- js/common.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){var o=$(".owl-carousel");o.owlCarousel({items:1,loop:!0,nav:!1,dots:!1}),$("#NextBtn").click(function(){o.trigger("next.owl.carousel",[150])}),$("#PrevBtn").click(function(){o.trigger("prev.owl.carousel",[150])})});
